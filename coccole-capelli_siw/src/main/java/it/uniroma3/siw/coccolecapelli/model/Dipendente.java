cb96a26d2c9abecd594781082ac30273f9c5ebae
giug2 <giulia.gichi@gmail.com>
2023-05-13 16:25:40 +0200

    Update Dipendente.java

--- coccole-capelli_siw/src/main/java/it/uniroma3/siw/coccolecapelli/model/Dipendente.java
+++ coccole-capelli_siw/src/main/java/it/uniroma3/siw/coccolecapelli/model/Dipendente.java
@@ -1,7 +1,18 @@
 package it.uniroma3.siw.coccolecapelli.model;
 
+
+import jakarta.persistence.*;
+
+@Entity
 public class Dipendente {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 	private String nome;
+	private String numTelefono;
+	private String specializzazione;
+	
 	
 }
