120de00055416b4dac01709573ef9b1314267c2e
giug2 <giulia.gichi@gmail.com>
2023-05-16 10:51:03 +0200

    va?

--- coccole-capelli_siw/src/main/java/it/uniroma3/siw/coccolecapelli/authentication/AuthConfiguration.java
+++ coccole-capelli_siw/src/main/java/it/uniroma3/siw/coccolecapelli/authentication/AuthConfiguration.java
@@ -1,93 +1,81 @@
 package it.uniroma3.siw.coccolecapelli.authentication;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
-import static it.uniroma3.siw.coccolecapelli.model.Credentials.ADMIN_ROLE;
-//import static it.uniroma3.siw.spring.model.Credentials.DEFAULT_ROLE;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-/**
- * The AuthConfiguration is a Spring Security Configuration.
- * It extends WebSecurityConfigurerAdapter, meaning that it provides the settings for Web security.
- */
 @Configuration
 @EnableWebSecurity
-public class AuthConfiguration extends WebSecurityConfigurerAdapter {
+//public  class WebSecurityConfig {
+	public class AuthConfiguration {
 
-    /**
-     * The datasource is automatically injected into the AuthConfiguration (using its getters and setters)
-     * and it is used to access the DB to get the Credentials to perform authentiation and authorization
-     */
+    private static final String ADMIN_ROLE = null;
+	@Autowired
+    private DataSource dataSource;
+
     @Autowired
-    DataSource datasource;
-
-    /**
-     * This method provides the whole authentication and authorization configuration to use.
-     */
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                // authorization paragraph: qui definiamo chi può accedere a cosa
-                .authorizeRequests()
-                // chiunque (autenticato o no) può accedere alle pagine index, login, register, ai css e alle immagini
-                .antMatchers(HttpMethod.GET, "/", "/index", "/login", "/register", "/css/**", "/images/**").permitAll()
-                // chiunque (autenticato o no) può mandare richieste POST al punto di accesso per login e register 
-                .antMatchers(HttpMethod.POST, "/login", "/register").permitAll()
-                // solo gli utenti autenticati con ruolo ADMIN possono accedere a risorse con path /admin/**
-                .antMatchers(HttpMethod.GET, "/admin/**").hasAnyAuthority(ADMIN_ROLE)
-                .antMatchers(HttpMethod.POST, "/admin/**").hasAnyAuthority(ADMIN_ROLE)
-                // tutti gli utenti autenticati possono accere alle pagine rimanenti 
-                .anyRequest().authenticated()
-
-                // login paragraph: qui definiamo come è gestita l'autenticazione
-                // usiamo il protocollo formlogin 
-                .and().formLogin()
-                // la pagina di login si trova a /login
-                // NOTA: Spring gestisce il post di login automaticamente
-                .loginPage("/login")
-                // se il login ha successo, si viene rediretti al path /default
-                .defaultSuccessUrl("/default")
-
-                // logout paragraph: qui definiamo il logout
-                .and().logout()
-                // il logout è attivato con una richiesta GET a "/logout"
-                .logoutUrl("/logout")
-                // in caso di successo, si viene reindirizzati alla /index page
-                .logoutSuccessUrl("/index")        
-                .invalidateHttpSession(true)
-                .clearAuthentication(true).permitAll();
-    }
-
-    /**
-     * This method provides the SQL queries to get username and password.
-     */
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
         auth.jdbcAuthentication()
-                //use the autowired datasource to access the saved credentials
-                .dataSource(this.datasource)
-                //retrieve username and role
-                .authoritiesByUsernameQuery("SELECT username, role FROM credentials WHERE username=?")
-                //retrieve username, password and a boolean flag specifying whether the user is enabled or not (always enabled in our case)
+                .dataSource(dataSource)
+                .authoritiesByUsernameQuery("SELECT username, role from credentials WHERE username=?")
                 .usersByUsernameQuery("SELECT username, password, 1 as enabled FROM credentials WHERE username=?");
     }
-
-    /**
-     * This method defines a "passwordEncoder" Bean.
-     * The passwordEncoder Bean is used to encrypt and decrpyt the Credentials passwords.
-     */
+    
     @Bean
-    PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    protected SecurityFilterChain configure(final HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .csrf().and().cors().disable()
+                .authorizeHttpRequests()
+//                .requestMatchers("/**").permitAll()
+                // chiunque (autenticato o no) può accedere alle pagine index, login, register, ai css e alle immagini
+                .requestMatchers(HttpMethod.GET,"/","/index","/register","/css/**", "/images/**", "favicon.ico").permitAll()
+        		// chiunque (autenticato o no) può mandare richieste POST al punto di accesso per login e register 
+                .requestMatchers(HttpMethod.POST,"/register", "/login").permitAll()
+                .requestMatchers(HttpMethod.GET,"/admin/**").hasAnyAuthority(ADMIN_ROLE)
+                .requestMatchers(HttpMethod.POST,"/admin/**").hasAnyAuthority(ADMIN_ROLE)
+        		// tutti gli utenti autenticati possono accere alle pagine rimanenti 
+                .anyRequest().authenticated()
+                // LOGIN: qui definiamo il login
+                .and().formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .defaultSuccessUrl("/success", true)
+                .failureUrl("/login?error=true")
+                // LOGOUT: qui definiamo il logout
+                .and()
+                .logout()
+                // il logout è attivato con una richiesta GET a "/logout"
+                .logoutUrl("/logout")
+                // in caso di successo, si viene reindirizzati alla home
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .clearAuthentication(true).permitAll();
+        return httpSecurity.build();
     }
 }
